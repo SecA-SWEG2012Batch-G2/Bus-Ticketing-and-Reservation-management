73590c810ede0adc94f75e2cc3fecaf12b21a1de
Abel Getahun <88226208+abelops@users.noreply.github.com>
2021-08-29 17:31:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -56,9 +56,59 @@ void Record_Emp_Info(employeeInfo empData){
                 }
 }
 };
+struct bus{
+    int BID; //BUS ID
+    string model; //BUS MODEL
+    string color; //BUS COLOR
+    string license;//BUS LICENSE
+    int EID;// EMPLOYEE ID
+    int distanceTraveled;//DISTANCE THE BUS TRAVELED
+    int MID;// MAINTAINANCE HISTORY
+    int inputBus(){
+        cout<<"[+] Enter the bus id: ";
+        cin>>BID;
+        cout<<"[+] Enter the bus model: ";
+        cin>>model;
+        cout<<"[+] Enter the bus color: ";
+        cin>>license;
+        cout<<"[+] Enter the bus driver employee ID: ";
+        cin>>EID;
+        cout<<"[+] Enter the distance traveled by bus: ";
+        cin>>distanceTraveled;
+        cout<<"[+] Enter the bus maintainance ID: ";
+        cin>>MID;
+    }
+
+};
+struct customer{
+    int CID; //CUSTOMER ID
+    string name;// CUSTOMER NAME
+    int age; //AGE OF THE CUSTOMER
+    string identificationCard; //ID NUMBER OF THE CUSTOMER
+    int balance; //THE BALANCE OF THE CUSTOMER
+    char gender;//THE GENDER OF THE CUSTOintMER
+    int travelDistance; //THE DISTANCE THAT THE CUSTOMER COVERED BY THE COMPANY SERVICE
+    void inputCustomer(){
+        cout<<"[+] Enter the customer ID: ";
+        cin>>this->CID;
+        cout<<"[+] Enter the customer name: ";
+        cin>>this->name;
+        cout<<"[+] Enter the customer age: ";
+        cin>>this->age;
+        cout<<"[+] Enter the customer balance: ";
+        cin>>this->balance;
+        cout<<"[+] Enter the customer gender: ";
+        cin>>this->gender;
+        cout<<"[+] Enter the customer traveled Distance: ";
+        cin>>this->travelDistance;
+    }
+};
 int main(){
 employeeInfo empData;
 empData.Record_Emp_Info(empData);
-
+bus busInfo;
+customer customerInfo;
+busInfo.inputBus();
+customerInfo.inputCustomer();
 return 0;
 }
