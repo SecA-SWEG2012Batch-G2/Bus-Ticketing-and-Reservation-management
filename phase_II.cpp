95a6720597cdd36a111bbc7f7c4148af3636605d
Abel Abebe <88794322+Abelabebe313@users.noreply.github.com>
2021-09-23 21:59:05 +0300

    Update phase_II.cpp

--- phase_II.cpp
+++ phase_II.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <windows.h>
+#include<iomanip>
+#include<vector>
+#include<string.h>
+#include<bits/stdc++.h>
 
+
+using namespace std;
+
+void insertRecordMenu();//prototyping
+void adminMenu();//prototyping
+void displayRecord();//prototyping
+void deleteRecordMenu();//prototyping
+void rechargeBal();
+void book();
+bool ser(string& h, string& term){
+    if(h.find(term) != string::npos){
+       return true;
+       }
+    else{
+        return false;
+    }
+}
